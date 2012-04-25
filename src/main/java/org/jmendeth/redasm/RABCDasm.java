bb9cc224f40d2b7934be8de1df29844015e7a5cf
Xavier Mendez <jmendeth@gmail.com>
2012-04-25 13:55:35 +0200

    Add support for relative paths

--- src/main/java/org/jmendeth/redasm/RABCDasm.java
+++ src/main/java/org/jmendeth/redasm/RABCDasm.java
@@ -20,9 +20,9 @@ public class RABCDasm {
     public static final String DISASSM_COMMAND = "rabcdasm";
     public static final String ASSM_COMMAND    = "rabcasm";
 
-    public static List<File> export(File swf, File pref) throws IOException, RABCDasmException {
-        runCommand(EXPORT_COMMAND, "Error when exporting ABC blocks.",
-                swf.getPath(), pref.getPath());
+    public static List<File> export(File swf, File pref, File root) throws IOException, RABCDasmException {
+        runCommand(EXPORT_COMMAND, "Error when exporting ABC blocks.", root,
+                getCPath(swf,root),getCPath(pref,root));
         List<File> ret = new ArrayList<>();
         for (int i=0; true; i++) {
             File ch = new File(pref.getPath()+i+".abc");
@@ -32,35 +32,34 @@ public class RABCDasm {
         return ret;
     }
 
-    public static void replace(File swf, int idx, File abc) throws IOException, RABCDasmException {
-        runCommand(REPLACE_COMMAND, "Error when replacing the block "+idx+".",
-                swf.getPath(), String.valueOf(idx), abc.getPath());
+    public static void replace(File swf, int idx, File abc, File root) throws IOException, RABCDasmException {
+        runCommand(REPLACE_COMMAND, "Error when replacing the block "+idx+".", root,
+                getCPath(swf,root), String.valueOf(idx), getCPath(abc,root));
     }
 
-    public static File disassemble(File abc, File dir) throws IOException, RABCDasmException {
-        runCommand(DISASSM_COMMAND, "Error when disassembling ABC block.",
-                abc.getPath(), dir.getPath());
+    public static File disassemble(File abc, File dir, File root) throws IOException, RABCDasmException {
+        runCommand(DISASSM_COMMAND, "Error when disassembling ABC block.", root,
+                getCPath(abc,root), getCPath(dir,root));
         final String main = dir.getName()+".main.asasm";
         return new File(dir, main);
     }
 
-    public static void assemble(File dirmain, File abc) throws IOException, RABCDasmException {
-        runCommand(ASSM_COMMAND, "Error when assembling ABC block.",
-                dirmain.getPath(), abc.getPath());
+    public static void assemble(File dirmain, File abc, File root) throws IOException, RABCDasmException {
+        runCommand(ASSM_COMMAND, "Error when assembling ABC block.", root,
+                getCPath(dirmain,root), getCPath(abc,root));
     }
 
-    public static Process executeCommand(String command, String... args) throws IOException {
+    public static Process executeCommand(String command, String[] args, File wdir) throws IOException {
         List<String> cm = new ArrayList<>(Arrays.asList(args));
         cm.add(0, command);
-        ProcessBuilder pb = new ProcessBuilder(cm);
-        return pb.start();
+        return new ProcessBuilder(cm).directory(wdir).start();
     }
 
     public static final String STACKTRACE_SEPARATOR = "----------------";
-    public static void runCommand(String command, String msg, String... args) throws IOException, RABCDasmException {
+    public static void runCommand(String command, String msg, File wdir, String... args) throws IOException, RABCDasmException {
         try {
             final StringWriter out = new StringWriter();
-            final Process pr = executeCommand(command, args);
+            final Process pr = executeCommand(command, args, wdir);
             Thread errcp = new Thread(new Runnable() {
 
                 @Override
@@ -105,10 +104,15 @@ public class RABCDasm {
             List<String> lines = new ArrayList<>(Arrays.asList(outs.split("\n")));
             String msgS = StringUtils.join(lines.subList(0, lines.indexOf(STACKTRACE_SEPARATOR)), "\n");
             msgS = msgS.substring(msgS.indexOf(":")+2);
-            throw new RABCDasmException(status, outs, msg+ansi().newline().bold().a(msgS).boldOff());
+            throw new RABCDasmException(status, outs, msg+ansi().newline().bold().a(msgS.replace("\t", "  ")).boldOff());
         } catch (IOException ex) {
             throw new IOException("When executing "+command+": "+ex.getMessage(), ex);
         }
+    }
+
+    public static String getCPath(File f, File root) {//FIXME: we should definitively migrate to Path
+        if (root != null) f = root.toPath().relativize(f.toPath()).toFile();
+        return f.getPath();
     }
 
 }
